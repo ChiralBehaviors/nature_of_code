64ab5cb6cc247f630a8bd7cb37fde1078cf8a756
Hallie Parry <halloran.parry@gmail.com>
2014-04-11 09:49:45 -0700

    Perlin2DNoise - cloud animation

---
+++ src/main/java/com/chiralbehaviors/natureofcode/perlin/Perlin2DNoise.java
@@ -0,0 +1,46 @@
+/** 
+ * (C) Copyright 2014 Chiral Behaviors, LLC. All Rights Reserved
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+package com.chiralbehaviors.natureofcode.perlin;
+
+import processing.core.PApplet;
+
+/**
+ * @author hparry
+ *
+ */
+public class Perlin2DNoise extends PApplet {
+	
+	float xoff = 0;
+	public void setup() {
+		size(640, 360);
+		
+	}
+	
+	public void draw() {
+		loadPixels();
+		for (int x = 0; x < width; x++) {
+			float yoff = 0;
+			for (int y = 0; y < height; y++) {
+				float shade = map(noise(xoff,yoff), 0, 1, 0, 255);
+				pixels[x + y*width] = color(shade);
+				yoff += 0.01;
+			}
+			xoff += 0.01;
+		}
+		updatePixels();
+	}
+
+}
--- src/main/java/com/chiralbehaviors/natureofcode/perlin/PerlinWalker.java
+++ src/main/java/com/chiralbehaviors/natureofcode/perlin/PerlinWalker.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and 
  * limitations under the License.
  */
-package com.chiralbehaviors.natureofcode.walker;
+package com.chiralbehaviors.natureofcode.perlin;
 
 import processing.core.PApplet;
 import processing.core.PVector;
